fe13aea90b424b79843bb152fc78e8175b6f2445
Frederico Kueckelhaus <freddy.bdb@gmail.com>
2020-12-11 18:49:22 +0100

    moving out of sync

--- Week 2/Kitty_ticker/script.js
+++ Week 2/Kitty_ticker/script.js
@@ -1,8 +1,15 @@
 // Grab all the kiities
 
 var kitties = document.querySelectorAll('#kitties img');
+var dots = document.getElementsByClassName('dot');
+
 //  Kiity counter
 var currentKitty = 0;
+// Timer
+var timer;
+// Transition
+var isInTransition = false;
+
 // functions
 
 function remove() {
@@ -14,24 +21,51 @@ function exitStage() {
 function addOnScreen() {
     kitties[currentKitty].classList.add('onscreen');
 }
+
+// Dot functions
+
+function dotOff() {
+    dots[currentKitty].classList.remove('on');
+}
+function dotOn() {
+    dots[currentKitty].classList.add('on');
+}
 ///////////////// Main function
 function moveKitties() {
+    isInTransition = true;
     remove();
     exitStage();
+    dotOff();
+
     currentKitty++;
     if (currentKitty >= kitties.length) {
         currentKitty = 0;
     }
+
     addOnScreen();
+    dotOn();
     // this moves the kittens again
-    setTimeout(moveKitties, 5000);
+    timer = setTimeout(moveKitties, 5000);
 }
 // This starts the kittens
-setTimeout(moveKitties, 2000);
+timer = setTimeout(moveKitties, 2000);
 
+// This takes out the exit left class and brings it to the right
 document.addEventListener('transitionend', function (e) {
     // console.log(e.target);
     if (e.target.className === 'exit-left') {
         e.target.classList.remove('exit-left');
     }
+    isInTransition = false;
 });
+
+for (var i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', getClickHandler(i));
+}
+
+function getClickHandler(i) {
+    return function () {
+        clearTimeout(timer);
+        moveKitties(i);
+    };
+}
