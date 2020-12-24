1587f3dfcc81ea8d6abd93609644a5b6266c9a0e
Frederico Kueckelhaus <freddy.bdb@gmail.com>
2020-12-24 17:07:40 +0100

    100% vanilla JS, need better XHRrejections and css positioning

--- spotify/script.js
+++ spotify/script.js
@@ -9,6 +9,9 @@ document.querySelector('.fa-github-square').addEventListener('click', () => {
 });
 
 var resultsContainer = document.querySelector('.resultsContainer');
+var moreBtn = document.getElementById('moreBtn');
+var prevBtn = document.getElementById('prevBtn');
+var headResults = document.getElementById('headResults');
 
 document.addEventListener('keydown', function (e) {
     if (e.code === 'Enter') {
@@ -22,11 +25,16 @@ document.getElementById('submitBtn').addEventListener('click', function () {
 });
 
 function submit() {
-    var media = document.querySelector('.media');
-    if (resultsContainer.hasChildNodes()) {
-        media.parentNode.innerHTML = '';
+    function cleanUp() {
+        var media = document.querySelector('.media');
+        if (resultsContainer.hasChildNodes()) {
+            media.parentNode.innerHTML = '';
+            moreBtn.style.visibility = 'hidden';
+        }
     }
+    cleanUp();
     if (document.getElementById('searchText').value === '') {
+        headResults.style.visibility = 'hidden';
         var alertBad = `<div class="alert alert-dismissible alert-danger">
   <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
 </div>`;
@@ -38,46 +46,99 @@ function submit() {
     var userInput = encodeURIComponent(
         document.getElementById('searchText').value.toLowerCase()
     );
+
+    var userInput2 = document.getElementById('searchText').value.toUpperCase();
+
     var artistOrAlbum = encodeURIComponent(
         document.getElementById('dropDown').value.toLowerCase()
     );
 
-    handle();
-    function handle() {
+    function head() {
+        headResults.innerHTML = `<i class="fa fa-headphones" aria-hidden="true"></i> SHOWING
+                RESULTS FOR : ${userInput2}`;
+        headResults.style.visibility = 'visible';
+    }
+    function noHead() {
+        headResults.innerHTML = `<i class="fa fa-headphones" aria-hidden="true"></i> ${userInput} didn't turn up any results <i class="fas fa-sad-cry"></i>`;
+        headResults.style.visibility = 'visible';
+    }
+
+    var searchUrl = `https://spicedify.herokuapp.com/spotify?query=${userInput}&type=${artistOrAlbum}`;
+
+    handle(searchUrl);
+    function handle(url) {
         var xhr = new XMLHttpRequest();
-        xhr.open(
-            'GET',
-            `https://spicedify.herokuapp.com/spotify?query=${userInput}&type=${artistOrAlbum}`,
-            true
-        );
+        xhr.open('GET', url, true);
         xhr.onload = function () {
             if (this.status == 200) {
                 var response = JSON.parse(this.response);
                 if (artistOrAlbum === 'artist') {
-                    createNew(response);
+                    if (response.artists.items.length === 0) {
+                        noHead();
+                    } else {
+                        head();
+                        createNew(response);
+                    }
                 } else if (artistOrAlbum === 'album') {
-                    console.log(response);
-                    createNewAlbum(response);
+                    if (response.albums.items.length === 0) {
+                        noHead();
+                    } else {
+                        head();
+                        // console.log(response);
+                        createNewAlbum(response);
+                    }
                 }
-            } else {
+            } else if (this.status == 404) {
                 console.log('OUR servers are down');
             }
         };
         xhr.send();
     }
-}
 
-function createNew(response) {
-    for (var i = response.artists.items.length - 1; i >= 0; i--) {
-        var artistName = response.artists.items[i].name;
-        var genres = response.artists.items[i].genres;
-        var followers = response.artists.items[i].followers.total;
-        var picture = '/brett-jordan-yBBrTuOwhNA-unsplash.jpg';
-        if (response.artists.items[i].images.length > 0) {
-            picture = response.artists.items[i].images[0].url;
+    function createNew(response) {
+        cleanUp();
+        var nextUrl =
+            response.artists.next &&
+            response.artists.next.replace(
+                'api.spotify.com/v1/search',
+                'spicedify.herokuapp.com/spotify'
+            );
+        if (nextUrl != null) {
+            // console.log(nextUrl);
+            moreBtn.style.visibility = 'visible';
+            moreBtn.addEventListener('click', () => {
+                handle(nextUrl);
+            });
+        } else if (nextUrl == null) {
+            moreBtn.style.visibility = 'hidden';
         }
-        var link = response.artists.items[i].external_urls.spotify;
-        var result = `
+        var prevUrl =
+            response.artists.previous &&
+            response.artists.previous.replace(
+                'api.spotify.com/v1/search',
+                'spicedify.herokuapp.com/spotify'
+            );
+        if (prevUrl != null) {
+            // console.log(nextUrl);
+            prevBtn.style.visibility = 'visible';
+            prevBtn.addEventListener('click', () => {
+                handle(prevUrl);
+                cleanUp();
+            });
+        } else if (prevUrl == null) {
+            prevBtn.style.visibility = 'hidden';
+        }
+        for (var i = response.artists.items.length - 1; i >= 0; i--) {
+            var artistName = response.artists.items[i].name;
+            var genres = response.artists.items[i].genres;
+            var followers = response.artists.items[i].followers.total;
+            var picture =
+                'https://images.pexels.com/photos/3391933/pexels-photo-3391933.jpeg?cs=srgb&dl=pexels-miguel-á-padriñán-3391933.jpg&fm=jpg';
+            if (response.artists.items[i].images.length > 0) {
+                picture = response.artists.items[i].images[0].url;
+            }
+            var link = response.artists.items[i].external_urls.spotify;
+            var result = `
         <a href="${link}"
         <div class="media">
                 <img
@@ -93,21 +154,55 @@ function createNew(response) {
                 </div>
             </div></a>`;
 
-        resultsContainer.insertAdjacentHTML('afterbegin', result);
-    }
-}
-
-function createNewAlbum(response) {
-    for (var i = response.albums.items.length - 1; i >= 0; i--) {
-        var albumName = response.albums.items[i].name;
-        var link = response.albums.items[i].external_urls.spotify;
-        var picture = '/brett-jordan-yBBrTuOwhNA-unsplash.jpg';
-        if (response.albums.items[i].images.length > 0) {
-            picture = response.albums.items[i].images[0].url;
+            resultsContainer.insertAdjacentHTML('afterbegin', result);
         }
-        var release = response.albums.items[0].release_date;
-        var artist = response.albums.items[0].artists[0].name;
-        var result = `
+    }
+
+    function createNewAlbum(response) {
+        cleanUp();
+        var nextUrl =
+            response.albums.next &&
+            response.albums.next.replace(
+                'api.spotify.com/v1/search',
+                'spicedify.herokuapp.com/spotify'
+            );
+        if (nextUrl != null) {
+            // console.log(nextUrl);
+            moreBtn.style.visibility = 'visible';
+            moreBtn.addEventListener('click', () => {
+                handle(nextUrl);
+            });
+        } else if (nextUrl == null) {
+            moreBtn.style.visibility = 'hidden';
+        }
+        var prevUrl =
+            response.albums.previous &&
+            response.albums.previous.replace(
+                'api.spotify.com/v1/search',
+                'spicedify.herokuapp.com/spotify'
+            );
+        if (prevUrl != null) {
+            // console.log(nextUrl);
+            prevBtn.style.visibility = 'visible';
+            prevBtn.addEventListener('click', () => {
+                handle(prevUrl);
+                cleanUp();
+            });
+        } else if (prevUrl == null) {
+            prevBtn.style.visibility = 'hidden';
+        }
+
+        for (var i = response.albums.items.length - 1; i >= 0; i--) {
+            var albumName = response.albums.items[i].name;
+            var link = response.albums.items[i].external_urls.spotify;
+            var picture =
+                'https://images.pexels.com/photos/3391933/pexels-photo-3391933.jpeg?cs=srgb&dl=pexels-miguel-á-padriñán-3391933.jpg&fm=jpg';
+            if (response.albums.items[i].images.length > 0) {
+                picture = response.albums.items[i].images[0].url;
+            }
+            var release = response.albums.items[0].release_date;
+            var artist = response.albums.items[0].artists[0].name;
+            var result = `
         <a href="${link}"
         <div class="media">
                 <img
@@ -118,11 +213,12 @@ function createNewAlbum(response) {
                 <div class="media-body">
                     <h5 class="mt-0">${albumName}</h5>
                     <p>Release: ${release}</p>
-                    <p class="mb-0">Followers: ${artist}
+                    <p class="mb-0">Artist: ${artist}
                     </p>
                 </div>
             </div></a>`;
 
-        resultsContainer.insertAdjacentHTML('afterbegin', result);
+            resultsContainer.insertAdjacentHTML('afterbegin', result);
+        }
     }
 }
