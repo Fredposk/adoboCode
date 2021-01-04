65384526c2edde9f96122c05369cfc615864980b
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-01-04 11:33:58 +0100

    regEx and fetchAPI

--- Week 3/directory_Tue/script2.js
+++ Week 3/directory_Tue/script2.js
@@ -1,24 +1,30 @@
 const searchInput = document.querySelector('.form');
 const results = document.querySelector('.results');
-searchInput.addEventListener('change', getValue);
-searchInput.addEventListener('keyup', getValue);
+searchInput.addEventListener('change', display);
+searchInput.addEventListener('keyup', display);
 // From the input it will run a function to fetch data and change query
 
+let endpoint = `https://spicedworld.herokuapp.com/?q=${searchInput.value}`;
 const countries = [];
-function getValue() {
-    // console.log(searchInput.value);
-    let endpoint = `https://spicedworld.herokuapp.com/?q=${searchInput.value}`;
 
+// console.log(searchInput.value);
+
+const fetch = () => {
     fetch(endpoint)
         .then((blob) => blob.json())
         .then((data) => countries.push(...data));
-
-    console.log(findMatches(searchInput, countries));
-}
+};
+// console.log(findMatches(searchInput, countries));
 
 function findMatches(wordToMatch, countries) {
     return countries.filter((place) => {
         const regex = new RegExp(wordToMatch, 'gi');
         return place.match(regex);
     });
+}
+
+function display() {
+    fetch;
+    const match = findMatches(this.value, countries);
+    console.log(match);
 }
