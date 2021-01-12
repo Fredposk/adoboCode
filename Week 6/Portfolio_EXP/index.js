53d5c0b1ed30c4210616ca1f392c31fe875ed41b
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-01-12 22:40:25 +0100

    Update index.js
    adjustments

--- Week 6/Portfolio_EXP/index.js
+++ Week 6/Portfolio_EXP/index.js
@@ -14,15 +14,7 @@ const PORT = process.env.PORT || 8081;
 // Middleware
 app.use(morgan('tiny'));
 app.use(require('cookie-parser')());
-
-app.use((req, res, next) => {
-    if (!res.cookie('name', 'CookieCrisp')) {
-        res.redirect('/cookie');
-        next();
-    } else {
-        next();
-    }
-});
+app.use(cookie);
 
 app.use(express.static(path.join(__dirname, 'projects')));
 app.use(express.static('./public'));
@@ -41,12 +33,28 @@ app.get('/cookie', (req, res) => {
 app.post('/cookie', (req, res) => {
     const { subscribe } = req.body;
     if (subscribe) {
-        res.cookie('name', 'CookieCrisp');
+        res.cookie('CookieCrisp', true);
         res.send(`<h1>WELCOME TO ASTROWORLD</h1>`);
     } else {
-        // res.redirect('/cookie');
         res.send(`<h1>Must accept cookies to continue</h1>`);
     }
 });
+
+const cookie = () => {
+    app.use((req, res, next) => {
+        let local = req.url;
+        if (req.url == '/cookie') {
+            console.log(local);
+            next();
+        } else {
+            if (!res.cookie('CookieCrisp')) {
+                console.log('thing');
+                res.redirect('./cookie');
+            } else {
+                next();
+            }
+        }
+    });
+};
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}}`));
