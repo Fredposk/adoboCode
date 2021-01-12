ada887de10db6a9872bb71e16fe5885413bd99a4
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-01-12 17:12:23 +0100

    Update indexEx.js
    express

--- Week 6/portfolio/indexEx.js
+++ Week 6/portfolio/indexEx.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'projects')));
 
 const PORT = process.env.PORT || 8081;
 
