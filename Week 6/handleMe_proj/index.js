47d11a36c87a400d9a8ecfbd977edead817e7c1c
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-01-14 21:37:51 +0100

    in-progress, need partials overhaul

--- Week 6/handleMe_proj/index.js
+++ Week 6/handleMe_proj/index.js
@@ -7,13 +7,37 @@ const projects = require('./projects.json');
 const app = express();
 // Logging middleware
 app.use(morgan('tiny'));
+
 // Rendering Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+// Filter projects
+const projFilter = (req) => (projects) =>
+    projects.directory === req.params.project;
+
+app.get('/projects/:project', (req, res) => {
+    const found = projects.some(projFilter(req));
+    if (found) {
+        // console.log(req.params);
+        const projectArray = projects.filter(projFilter(req));
+        const [{ title, directory }] = projectArray;
+        res.render('description', {
+            title: `${title}`,
+            projectArray,
+            link: directory,
+        });
+    } else {
+        // render 404 page
+        console.log('404 not found');
+    }
+});
+
 // Render / Homepage
 app.get('/', (req, res) => {
     // console.log('showhomepage');
     res.render('index', {
+        title: 'Home',
         projects,
     });
 });
