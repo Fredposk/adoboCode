795b2fb5ca23edbfe24d28b5bcbce46c59cf0583
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-01-19 19:39:51 +0100

    finished all

--- Week 7/testing/exercises/2/albums.test.js
+++ Week 7/testing/exercises/2/albums.test.js
@@ -1,10 +1,26 @@
-const { getAlbumNames } = require("./albums");
-const spotify = require("./spotify");
+const { getAlbumNames } = require('./albums');
+const spotify = require('./spotify');
 
-jest.mock("./spotify");
+jest.mock('./spotify');
 
-test("album names are in alphabetical order", () => {
-    return getAlbumNames("meat loaf").then((albumNames) => {
+test('album names are in alphabetical order', () => {
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [
+                {
+                    name: 'meat loaf',
+                },
+                {
+                    name: 'meet loaf',
+                },
+                {
+                    name: 'moot loaf',
+                },
+            ],
+        },
+    });
+
+    return getAlbumNames('meat loaf').then((albumNames) => {
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
 });
